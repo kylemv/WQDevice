9d41bddf1bc17ea9350c5bfc6e87ba3fb97a3ef9
Kevin Young <kyoung18@terpmail.umd.edu>
2015-11-28 20:48:27 -0500

    adding fona + sensors

--- datalogger.ino.ino
+++ datalogger.ino.ino
@@ -20,44 +20,62 @@
 #include <Math.h>
 #include <Time.h>
 
-#define turbidityPin A1 // turbidity sensor analog output to Arduino Analog Input 1
+// FONA.....................................
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
+char replybuffer[255]; // this is a large buffer for replies
+#include <SoftwareSerial.h>
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+uint8_t type;
+/* ..................................................................... */
 
-// pH .....................................
+
+// TURBIDITY ..............................
+#define turbidityPin A1 // turbidity sensor analog output to Arduino Analog Input 1
+#define turbidityOffset 0
+#define thermistorPin A2
+/*.........................................................................*/
+
+
+// pH ....................................
 #define phPin A0        // pH meter Analog output to Arduino Analog Input 0
 #define Offset 0.00     // deviation compensate
 #define samplingInterval 20
 #define printInterval 800
 #define ArrayLength 40    // times of collection
 int pHArray[ArrayLength]; // Store the average value of the sensor feedback
-int pHArrayIndex = 0;    
+int pHArrayIndex = 0;
+/* .............................................................................*/
 
-#define FONA_RX 2
-#define FONA_TX 3
-#define FONA_RST 4
-
-#define LED 8
-
+// TEMPERATURE ..................................
 int tempPin = 7;
+# define tempOffset 17.0
+OneWire ds(tempPin);
+/*........................................................................*/
 
-DS1337 RTC; //Create RTC object for DS1337 RTC
+// INTERRUPTS................................
+DS1337 RTC; // Create RTC object for DS1337 RTC
 static uint8_t prevSecond=0; 
 static DateTime interruptTime;
-
 static uint16_t interruptInterval = 10; //Seconds. Change this to suitable value.
-OneWire ds(tempPin);
+/* ..............................................................................  */
 
+#define LED 8
 
 String data = "";
 float temp = -1.00; // in F
 float ph = -1.00;
-float tds = -1.00;
+float turbidity = -1.00;
+float thermistorTemp = -1.00;
 
 void setup () {
      /*Initialize INT0 pin for accepting interrupts */
      PORTD |= 0x04;
      DDRD &=~ 0x04;
-
-//     Wire.begin();
      Serial.begin(115200);
      RTC.begin();
      
@@ -67,12 +85,17 @@ void setup () {
      pinMode(10, OUTPUT);
      digitalWrite(10, HIGH);
      
-     // initialize digital pin 8 as an output for the LED.
-     pinMode(LED, OUTPUT);
+     fonaSerial->begin(4800);
+     if (! fona.begin(*fonaSerial)) {
+       Serial.println(F("Couldn't find FONA"));
+       while (1);
+     }
+     Serial.println(F("FONA is OK"));
+     Serial.print(F("Found "));
      
      Serial.print("Load SD card...");
      
-     // Check if SD card can be intialized.
+     // Check if SD card can be initialized.
      if (!SD.begin(10))  //Chipselect is on pin 10
      {
         Serial.println("SD Card could not be initialized, or not found");
@@ -89,85 +112,68 @@ void setup () {
      // or this
      DateTime  start = RTC.now();
      interruptTime = DateTime(start.get() + interruptInterval); //Add interruptInterval in seconds to start time
+     
+     // initialize digital pin 8 as an output for the LED.
+     pinMode(LED, OUTPUT);
 }
 
 void loop () {
-    
     float voltage;
     int BatteryValue;
  
     BatteryValue = analogRead(A7);
     voltage = BatteryValue * (1.1 / 1024)* (10+2)/2;  //Voltage divider
+    Serial.print("Battery: ");
+    Serial.println(voltage, DEC);
+    
     temp = getTemp(1);
-    Serial.println("Temp: ");
-    Serial.print(temp, DEC);
+    Serial.print("Temp: ");
+    Serial.println(temp, DEC);
+    
     ph = getpH();
-    Serial.println("pH: ");
-    Serial.print(ph, DEC);
-    tds = getTurbidity();
-    data = formPOSTRequest(ph, temp, tds);
- 
-    DateTime now = RTC.now(); //get the current date-time  
-    now.m = now.month() + 8;
-    now.yOff = now.year() - 2000 + 14;
-    now.d = now.date() + 16;
-    if((now.second()) !=  prevSecond )
-    {
-    //print only when there is a change
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.date(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.print("   ");
-    Serial.print(voltage);
-    Serial.print(" V");
-    Serial.println();
-
-    }
-     prevSecond = now.second();
+    Serial.print("pH: ");
+    Serial.println(ph, DEC);
+    
+    turbidity = getTurbidity();
+    Serial.print("turbidity: ");
+    Serial.println(turbidity, DEC);
+    
+    thermistorTemp = getThermistorTemp();
+    Serial.print("thermistorTemp: ");
+    Serial.println(thermistorTemp, DEC);
+    
+    DateTime now = RTC.now(); //get the current date-time
+    prevSecond = now.second();
     
     //|||||||||||||||||||Write to Disk|||||||||||||||||||||||||||||||||| 
     File logFile = SD.open("DATALOG.CSV", FILE_WRITE);
     Serial.println(logFile);
     if (logFile) {
-    Serial.println("printing to logfile");
-    // temp
-    logFile.print(temp, DEC);
-    logFile.print(',');
-    // pH
-    logFile.print(ph, DEC);
-    logFile.print(',');
-    // turbidity
-    // timestamp
-    logFile.print(now.year(), DEC);
-    logFile.print('/');
-    logFile.print(now.month(), DEC);
-    logFile.print('/');
-    logFile.print(now.date(), DEC);
-    logFile.print(',');
-    logFile.print(now.hour(), DEC);
-    logFile.print(':');
-    logFile.print(now.minute(), DEC);
-    logFile.print(':');
-    logFile.print(now.second(), DEC);
-    logFile.print(',');
-    logFile.println(voltage);
-    logFile.close();
-    digitalWrite(LED, HIGH);
-    delay(100);
-    digitalWrite(LED, LOW);
-    delay(100);
-    digitalWrite(LED, HIGH);
-    delay(100);
-    digitalWrite(LED, LOW);
-    delay(100);
+      Serial.println("printing to logfile");
+      // temp
+      logFile.print(temp, DEC);
+      logFile.print(',');
+      // pH
+      logFile.print(ph, DEC);
+      logFile.print(',');
+      // turbidity
+      logFile.print(turbidity, DEC);
+      logFile.print(',');
+      // timestamp
+      logFile.print(now.year(), DEC);
+      logFile.print('/');
+      logFile.print(now.month(), DEC);
+      logFile.print('/');
+      logFile.print(now.date(), DEC);
+      logFile.print(',');
+      logFile.print(now.hour(), DEC);
+      logFile.print(':');
+      logFile.print(now.minute(), DEC);
+      logFile.print(':');
+      logFile.print(now.second(), DEC);
+      logFile.print(',');
+      logFile.println(voltage);
+      logFile.close();
     }
 
     //|||||||||||||||||||Write to Disk||||||||||||||||||||||||||||||||||
@@ -176,51 +182,27 @@ void loop () {
     RTC.enableInterrupts(interruptTime.hour(),interruptTime.minute(),interruptTime.second());    // set the interrupt at (h,m,s)
     sleep_enable();      // Set sleep enable bit
     attachInterrupt(0, INT0_ISR, LOW);  //Enable INT0 interrupt (as ISR disables interrupt). This strategy is required to handle LEVEL triggered interrupt
-    
-    
-    ////////////////////////END : Application code //////////////////////////////// 
-   
-    
-    //\/\/\/\/\/\/\/\/\/\/\/\/Sleep Mode and Power Down routines\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+    delay(5000);
             
     //Power Down routines
     cli(); 
     sleep_enable();
     sleep_bod_disable(); // Disable brown out detection during sleep. Saves more power
     sei();
-//       
-    digitalWrite(4,HIGH); //Power Off SD Card.
+    digitalWrite(4,HIGH); // Power Off SD Card.
     
     Serial.println("\nSleeping");
     delay(10); //This delay is required to allow print to complete
     // Shut down all peripherals like ADC before sleep. Refer Atmega328 manual
     power_all_disable(); // This shuts down ADC, TWI, SPI, Timers and USART
-//    power_adc_disable();
-//    power_spi_disable();
-//    power_timer0_disable();
-//    power_timer1_disable();
-//    power_timer2_disable();
-//    power_twi_disable();
-//    power_usart0_disable();
     sleep_cpu();         // Sleep the CPU as per the mode set earlier(power down)
     sleep_disable();     // Wakes up sleep and clears enable bit. Before this ISR would have executed
     power_all_enable();  // This enables ADC, TWI, SPI, Timers and USART
     delay(10);         // This delay is required to allow CPU to stabilize
     Serial.println("Awake from sleep");    
     digitalWrite(4,LOW); //Power On SD Card.
-
-    //\/\/\/\/\/\/\/\/\/\/\/\/Sleep Mode and Power Saver routines\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
 } 
 
-  
-//Interrupt service routine for external interrupt on INT0 pin conntected to DS1337 /INT
-void INT0_ISR()
-{
-    // Keep this as short as possible. Possibly avoid using function calls
-    detachInterrupt(0); 
-    interruptTime = DateTime(interruptTime.get() + interruptInterval);  //decide the time for next interrupt, configure next interrupt  
-}
 
 // returns temp from one DS18B20 in C or F
 float getTemp(int units) { 
@@ -258,9 +240,9 @@ float getTemp(int units) {
     TemperatureSum = TemperatureSum * (9/5) + 32;
   }
   
-  Serial.print("Temp: ");
-  Serial.println(TemperatureSum);
-  return TemperatureSum;
+//  Serial.print("Temp: ");
+//  Serial.println(TemperatureSum);
+  return TemperatureSum + tempOffset;
 }
 
 // returns current pH
@@ -279,9 +261,9 @@ float getpH() {
   }
   if (millis() - printTime > printInterval) {   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
     Serial.print("Voltage:");
-    Serial.print(voltage,2);
-    Serial.print("    pH value: ");
-    Serial.println(pHValue,2);
+    Serial.println(voltage,2);
+//    Serial.print("    pH value: ");
+//    Serial.println(pHValue,2);
     digitalWrite(LED,digitalRead(LED)^1);
     printTime = millis();
   }
@@ -290,10 +272,23 @@ float getpH() {
 
 // returns current turbidity/tds
 float getTurbidity () {
-   int num = analogRead(turbidityPin);
-   float voltage = num/1023;
+   float sensorValue = analogRead(turbidityPin);
+   Serial.println(sensorValue);
+   float voltage = sensorValue * (5.0/1023.0);
+   Serial.print("Turbidity voltage: ");
+   Serial.println(voltage, DEC);
    float turbidity = -1.333*log(voltage) + 4.71;
    return turbidity;
+}
+
+float getThermistorTemp() {
+   float sensorValue = analogRead(turbidityPin);
+   float temp;
+   temp = log(10000.0*((1024.0/sensorValue-1)));
+   temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp ))* temp);
+   temp = temp - 273.15;            // Convert Kelvin to Celcius
+   temp = (temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+   return temp;
 }
 
 
